b8b9b6d768991080c2b7cba56951ed3c3d425203
bhagat121 <86947529+bhagat121@users.noreply.github.com>
2021-11-04 23:39:31 +0530

    Add files via upload

---
+++ src/Conn.java
@@ -0,0 +1,20 @@
+package BankManagementSystem;
+
+import java.sql.*;  
+
+public class Conn{
+    Connection c;
+    Statement s;
+    public Conn(){  
+        try{  
+            Class.forName("com.mysql.jdbc.Driver");  
+            c =DriverManager.getConnection("jdbc:mysql:///project4","root","");    
+            s =c.createStatement(); 
+           
+          
+            
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+}  
